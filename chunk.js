cfd5d02e576b4f1b64239b8734ae5aa68d246e6d
dkang417 <dkang417@gmail.com>
2019-02-19 21:09:08 -0500

    update

--- chunk.js
+++ chunk.js
@@ -8,7 +8,24 @@
 // chunk([1, 2, 3, 4, 5], 4) --> [[ 1, 2, 3, 4], [5]]
 // chunk([1, 2, 3, 4, 5], 10) --> [[ 1, 2, 3, 4, 5]]
 
+// create empty array to hold chunks called 'chunked'
+// for each element in the 'unchunked' array 
+    // retreive the last element in chunked
+    // if last element does not exist or if its length is equal to chunk size
+        // push a new chunk into 'chunked' with the current element
+    // else add the current element into chunk 
+
 function chunk(array, size) {
-   
+    const chunked = [];
+
+    for (let element of array) {
+        const last = chunked[chunked.length - 1];
+        if (!last || last.length === size) {
+            chunked.push([element]);
+        } else {
+            last.push(element);
+        }
+    }
+    return chunked;
 
 }
