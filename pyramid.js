8ddee1678f4cd19775b5ba4adde1f0ec52a57e0c
dkang417 <dkang417@gmail.com>
2019-02-27 21:02:47 -0500

    update

--- pyramid.js
+++ pyramid.js
@@ -43,17 +43,18 @@ function pyramid(n) {
 //       '  #  '
 //       ' ### '
 //       '#####'
-function pyramid(n, row = 0, level = '' ) {
+
+
+function pyramid(n, row = 0, level = '') {
     if (row === n) {
         return;
     }
-
     if (level.length === 2 * n - 1) {
         console.log(level);
         return pyramid(n, row + 1);
     }
     const midpoint = Math.floor((2 * n - 1) / 2);
-    let add; 
+    let add;
     if (midpoint - row <= level.length && midpoint + row >= level.length) {
         add = '#';
     } else {
@@ -61,4 +62,3 @@ function pyramid(n, row = 0, level = '' ) {
     }
     pyramid(n, row, level + add);
 }
-
