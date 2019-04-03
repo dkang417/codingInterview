0672a489d79fb9251e859dc45874f52281062fb0
dkang417 <dkang417@gmail.com>
2019-04-03 14:23:50 -0400

    update

--- tree/levelwidth.js
+++ tree/levelwidth.js
@@ -15,8 +15,8 @@
 // width = bf traversal 
 
 function levelWidth(root) {
+    const arr = [root, 's'];
     const counter = [0];
-    const arr = [root,'s'];
 
     while (arr.length > 1) {
         const node = arr.shift();
@@ -32,4 +32,19 @@ function levelWidth(root) {
     return counters;
 }
 
+function levelWidth(root) {
+    const arr = [root, 's'];
+    const counter = 0;
+    while (arr.length > 1) {
+        const node = arr.shift();
+        if (node === 's') {
+            counters.push(0);
+            arr.push('s');
+        } else {
+            arr.push(...node.children);
+            counters[counter.length - 1]++;
+        }
+    }
+    return counters; 
+}
 module.exports = levelwidth;
