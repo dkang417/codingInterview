369984910b60a30c2e6124fce186d5b17c432f72
dkang417 <dkang417@gmail.com>
2019-04-12 16:53:05 -0400

    update

--- bubblesort.js
+++ bubblesort.js
@@ -2,10 +2,14 @@
 // Implement bubbleSort, selectionSort, and mergeSort
 
 
-
+// takes in array of numbers- 
+// find the largest number in array
+// and drag it to the right side
+// so we need to shrink the window each inner loop
 function bubbleSort(arr) {
 // 2 nested for loops, n^2 complexity 
     for (let i = 0; i < arr.length; i++) {
+        // minus i minus 1 to restrict the window each iteration
         for (let j = 0; j < (arr.length-i-1); j++) {
             if (arr[j] > arr[j + 1]) {
                 const lesser = arr[j + 1];
@@ -17,6 +21,14 @@ function bubbleSort(arr) {
     return arr;
     
 }
+
+
+
+
+
+
+
+// selection sort 
 
 function selectionSort(arr) {
 
