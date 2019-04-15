99b156659a85f57b0e8a5583506571286b328c16
dkang417 <dkang417@gmail.com>
2019-04-14 21:35:19 -0400

    update

--- bubblesort.js
+++ bubblesort.js
@@ -30,13 +30,8 @@ function bubbleSort(arr) {
 }
 
 
-
-
-
-
-
 // selection sort 
-// prove me wrong sort
+// prove me wrong sort - index i is lowest number. if not swap.
 
 // from i = 0 < arr length  . indexMin
 // assume the elemet at index i is the lowest value
@@ -70,13 +65,34 @@ function selectionSort(arr) {
 
 
 
+
+
+// recursion solution with 2 functions 
+
 function mergeSort(arr) {
 
 
 }
 
-function merge(left, right) {
+// [-30,22] [0,97]
+// create results array
+// while there are still elements in both arrays
+    // if the first elem the left half is less than first in right half
+        // shift the element from left into a 'result' arr
+    // else 
+        // shift the element from right into a result arr
+// take everything from the arr that still has stuff init and put in results
 
+function merge(left, right) {
+    const results = [];
+    while (left.length && right.length) {
+        if (left[0] < right[0]) {
+            results.push(left.shift());
+        } else {
+            results.push(right.shift());
+        }
+    }
+    return [...results, ...left, ...right];
 
 }
 
