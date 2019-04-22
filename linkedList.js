14663fb96398335d90304962a641fef06b64ca33
dkang417 <dkang417@gmail.com>
2019-04-21 21:17:47 -0400

    update

--- linkedList.js
+++ linkedList.js
@@ -171,25 +171,37 @@ module.exports = { Node, LinkedList };
 
 
 
+
+
+
 // linked list FrontendMasters 
-
-
 class LinkedList {
     constructor() {
         this.head = { value, next: null };
         this.tail = this.head;
     }
-    
+
     insert(value) {
         const node = { value, next: null };
         this.tail.next = node;
         this.tail = node;
+
     }
-    remove() {
-        
+    // [3] -> [5]
+    // delete tail
+    removeLast() {
+        // loop and find the node before the tail
+        // node.next === this.tail 
+        let currentNode = this.head;
+        while (currentNode.next !== this.tail ) {
+            currentNode = currentNode.next;
+        }
+        currentNode.next = null;
+        this.tail = currentNode;
+        return currentNode;
     }
-    contains() {
-        
-    }
+
 }
+
+
 
