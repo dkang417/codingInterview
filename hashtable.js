4228842310fdb8bfcc3690a1e2063af04983c818
dkang417 <dkang417@gmail.com>
2019-04-23 16:43:37 -0400

    update

--- hashtable.js
+++ hashtable.js
@@ -16,16 +16,25 @@ class HashTable {
             this._storage[index] = [];
         }
         // [0,0,0,[]]  
-        this._storage[index].push([key, value])
+        this._storage[index].push([key, value]);
         // [0,0,0,['a',1], ['b',2]] 
     }
 
     remove(){
+        
 
     }
 
-    retrieve() {
+    retrieve(key) {
+        const index = this._hash(key, this._tableSize);
+        const arrayAtIndex = this._storage[index];
         
+        if (arrayAtIndex) {
+            for (let i = 0; i < arrayAtIndex.length; i++){
+                const keyValueArray = arrayAtIndex[i];
+                if (keyValueArray[0] === key) return keyValueArray[1];
+            }
+        }
     }
     // {string} str - the string to be hashed
     // {number} n - the size of the storage array
@@ -43,6 +52,6 @@ const myHT = new HashTable(25);
 myHT.insert('a', 1)
 myHT.insert('b', 2);
 // hashTable { _storage: [0,0,0,['a',1],['b',2] 0,0,0]}
-
+console.log(myHT);
 
 
