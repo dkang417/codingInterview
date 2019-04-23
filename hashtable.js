fa45ee4d1247bc3ee409b6091091960948f4becc
dkang417 <dkang417@gmail.com>
2019-04-23 16:23:57 -0400

    update

--- hashtable.js
+++ hashtable.js
@@ -1,24 +1,48 @@
-// array 
 
+// implements using an array
 
 class HashTable {
 
-    constructor() {
-        this.storage = [];
-        this.length = 0;
-        this.head = 0;
+    constructor(val) {
+        this._storage = [];
+        this._tableSize = val;
+       
     }
 
-
-    insert(record){
-        this.storage[this.length + this.head] = record;
-        this.length++;
+    insert(key, value){
+        const index = this._hash(key, this._tableSize);
+        
+        if (!this._storage[index]) {
+            this._storage[index] = [];
+        }
+        // [0,0,0,[]]  
+        this._storage[index].push([key, value])
+        // [0,0,0,['a',1], ['b',2]] 
     }
 
     remove(){
 
     }
+
+    retrieve() {
+        
+    }
+    // {string} str - the string to be hashed
+    // {number} n - the size of the storage array
+    // {number} - an integer between 0 and n
+    _hash(str, n) {
+        let sum = 0;
+        for (let i = 0; i < str.length; i++) {
+            sum += str.charCodeAt(i) * 3;
+        }
+        return sum * n;
+    }
 }
-// pass key and value
+
+const myHT = new HashTable(25); 
+myHT.insert('a', 1)
+myHT.insert('b', 2);
+// hashTable { _storage: [0,0,0,['a',1],['b',2] 0,0,0]}
+
 
 
