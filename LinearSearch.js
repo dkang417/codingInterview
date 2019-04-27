1ad9f886c8b2178a911ec40395f88bb999a75a99
dkang417 <dkang417@gmail.com>
2019-04-27 19:38:56 -0400

    update

--- LinearSearch.js
+++ LinearSearch.js
@@ -10,5 +10,20 @@ function linearSearch(list, item) {
     return index;
 }
 
-linearSearch([1, 4, 2, 90, 103], 90);
+
+// implement linear search( takes 2 arguments- list , 
+//  item you are looking for
+// linearSearch([1, 4, 2, 90, 103], 90);
+
+// forEach(listitem, i) 
+// index = -1;
+function linearSearch(list, item) {
+    let index = -1;
+    list.forEach((listItem, i) => {
+        if (listItem === item) {
+            index = i;
+        }
+    });
+    return index; 
+}
 
