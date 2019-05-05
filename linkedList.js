b3c70e0639e9de0bc7b3e0434f792cedde22122e
dkang417 <dkang417@gmail.com>
2019-05-05 16:57:06 -0400

    update

--- linkedList.js
+++ linkedList.js
@@ -2,12 +2,34 @@
 // Implement classes Node and Linked Lists
 // See 'directions' document
 
+
 class Node {
-    constructor(data, next = null) {
+    constructor(data,next=null) {
         this.data = data;
         this.next = next;
     }
 }
+class LinkedList {
+    constructor() {
+        this.head = null;
+    }
+    
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 class LinkedList {
