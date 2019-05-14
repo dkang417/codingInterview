b93506549b421ed7b2e7074334fb8decb39cabc8
dkang417 <dkang417@gmail.com>
2019-05-13 22:15:41 -0400

    update

--- tree/index.js
+++ tree/index.js
@@ -155,4 +155,8 @@ class Tree {
             fn(node);
         }
     }
+
+ 
 }
+
+
--- tree/levelwidth.js
+++ tree/levelwidth.js
@@ -35,3 +35,42 @@ function levelWidth(root) {
 
 
 module.exports = levelwidth;
+
+// Given:
+//     0
+//   / |  \
+// 1   2   3
+// |       |
+// 4       5
+// Answer: [1, 3, 2]
+
+// if question asks for width- we are thinking BreathFirst traversal
+
+// create 2 arrays 
+// counters array - the array we want to return 
+// arr  - the queue we use to iterate through our tree
+
+
+function levelWidth(root) {
+    const arr = [root, 's'];
+    const counters = [0];
+
+    while (arr.length > 1) {
+        const node = arr.shift();
+
+        // if we pull out s - add new element of 0 for new row 
+        // into counter, push 's' back into the end
+
+        if (node === 's') {
+            counters.push(0);
+            arr.push('s');
+        } else {
+            // push all of the children
+            // incremnt last number in counters array
+            arr.push(...node.children);
+            counters[counters.length - 1]++;
+        }
+    }
+    return counters;
+
+}
