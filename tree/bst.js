d812681ee5a78eb093fe88c6c1c60dd8d2808d20
dkang417 <dkang417@gmail.com>
2019-05-28 16:58:04 -0400

    update

--- tree/bst.js
+++ tree/bst.js
@@ -28,6 +28,32 @@ class Node{
             this.right.insert(data);
         }
         else if (data > this.data) {
+            this.right = new Node(data);
+        }
+    }
+    contains(data) {
+        
+    }
+}
+
+
+class Node{
+    constructor(data) {
+        this.data = data;
+        this.left = null;
+        this.right = null;
+    }
+    insert(data) {
+        if (data < this.data && this.left) {
+            this.left.insert(data);
+        }
+        else if (data < this.data) {
+            this.left = new Node(data);
+        }
+        else if (data > this.data && this.right) {
+            this.right.insert(data);
+        }
+        else if (data > this.data) {
             this.right= new Node(data);
         }
     }
@@ -94,6 +120,22 @@ class Node {
    
 
 }
+function validate(node, min = null, nax = null) {
+    if (min !== null && node.data < min) {
+        return false;
+    }
+    if (max !== null && node.data > max) {
+        return false;
+    }
+    if (node.left && !validate(node.left, min, node.data)) {
+        return false;
+    }
+    if (node.right && !validate(node.right, node.data, max)) {
+        return false;
+    }
+    return true;
+}
+
 
 // validate if binary search tree 
 // recursion solution 
