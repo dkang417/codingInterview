221cec83900ae942259a8225b6c6c5309ccc2396
dkang417 <dkang417@gmail.com>
2019-06-01 14:41:53 -0400

    update

--- fib.js
+++ fib.js
@@ -187,4 +187,40 @@ function slowFib(n) {
 }
 const fib = memoize(slowFib);
 
+function fib(n) {
+    const arr = [0, 1];
+    for (let i = 2; i <= n; i++){
+        const a = arr[arr.length - 1];
+        const b = arr[arr.length - 2];
+        arr.push(a + b);
+    }
+    return arr[arr.length - 1];
+}
 
+function fib(n) {
+    if (n < 2) {
+        return n;
+    }
+    return fib(n - 1) + fib(n - 2);
+}
+
+
+
+function memoize(fn) {
+    const cache = {};
+    return function (...args) {
+        if (cache[args]) {
+            return cache[args];
+        }
+        const result = fn.apply(this, args);
+        cache[args] = result;
+        return result;
+    }
+}
+function slowFib(n) {
+    if (n < 2) {
+        return n;
+    }
+    return fib(n - 1) + fib(n - 2);
+}
+const fib = memoize(slowFib);
