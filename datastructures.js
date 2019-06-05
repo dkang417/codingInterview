82284495f4bf10fadd3030bd5cff6b91cf809957
dkang417 <dkang417@gmail.com>
2019-06-05 17:02:45 -0400

    update

--- datastructures.js
+++ datastructures.js
@@ -31,5 +31,24 @@ class SinglyLinkedList{
         this.length++;
         return this;
     }
+    pop() {
+        if (!this.head) {
+            return undefined;
+        }
+        const current = this.head;
+        const newTail = current;
+        while (current.next) {
+            newTail = current;
+            current = current.next;
+        }
+        this.tail = newTail;
+        this.tail.next = null;
+        this.length--;
+        if (this.length === 0) {
+            this.head = null;
+            this.tail = null;
+        }
+        return current;
+    }
 }
 const list = new SinglyLinkedList();
