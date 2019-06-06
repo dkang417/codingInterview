d0b877d713d35013c03b3943768aff6864e78006
dkang417 <dkang417@gmail.com>
2019-06-06 16:36:57 -0400

    update

--- datastructures.js
+++ datastructures.js
@@ -13,12 +13,17 @@ class Node {
     }
 }
 
+// need length, head, tail 
 class SinglyLinkedList{
     constructor() {
         this.length = 0;
         this.tail = null;
         this.head = null;
     }
+
+    // create a newnode with val
+    // if no head - head becomes newnode amd tail becomes head
+    // else - tail.next becomes newnode and tail becomes newnode
     push(val) {
         const newNode = new Node(val);
         if (!this.head) {
@@ -30,6 +35,32 @@ class SinglyLinkedList{
         }
         this.length++;
         return this;
+    }
+   
+    // if there are no nodes return undefined
+    // loop through list until you reach tail
+    // see the next property of the 2nd to last node to be null
+    // set the tail to be the 2nd to last node 
+    // decrement the length by 1
+    // return value of removed node 
+    pop() {
+        if (!this.head) {
+            return undefined;
+        }
+        const current = this.head;
+        const newTail = current; 
+        while (current.next) {
+            newTail = current;
+            current = current.next;
+        }
+        this.tail = newTail;
+        this.tail.next = null;
+        this.length--;
+        if (this.length === 0) {
+            this.head = null;
+            this.tail = null;
+        }
+        return current;
     }
     pop() {
         if (!this.head) {
@@ -50,5 +81,7 @@ class SinglyLinkedList{
         }
         return current;
     }
+
 }
+
 const list = new SinglyLinkedList();
