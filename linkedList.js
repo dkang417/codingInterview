dd1bd43e0750bb4a31e609cd214f037f68eb8e59
dkang417 <dkang417@gmail.com>
2019-10-27 20:16:37 -0400

    update

--- linkedList.js
+++ linkedList.js
@@ -85,6 +85,34 @@ class LinkedList {
             node = node.next;
         }
     }
+    getLast() {
+        if (!this.head) {
+            return null;
+        }
+
+        let node = this.head;
+        while (node) {
+            if (!node.next) {
+                return node;
+            }
+            node = node.next;
+        }
+    }
+    removeLast() {
+        if (!this.head) {
+            return null;
+        }
+        if (!this.head.next) {
+            this.head = null;
+        }
+        let prev = this.head;
+        let node = this.head.next;
+        while (node.next) {
+            prev = node;
+            node = node.next;
+        }
+        prev.next = null;
+    }
 
 }
 
