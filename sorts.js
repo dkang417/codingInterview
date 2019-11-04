5815a0963144ed6a1509bacf789773e6b45a35a1
dkang417 <dkang417@gmail.com>
2019-11-03 19:38:47 -0500

    update

--- sorts.js
+++ sorts.js
@@ -85,6 +85,7 @@ function insertionSort(arr) {
 }
 
 
+
 // exploits fact that arrays of 1 or 0 is already sorted
 // works by decomposing arrays into smaller arrays of 0 or 1 then merge them 
 // big o of mergesort-  O(n log n) 
@@ -129,46 +130,20 @@ function mergeSort(arr) {
     return merge(mergeSort(left), mergeSort(right));
 }
 
-
-// quick sort 
-
-// find where the pivot should end up in the selected array 
-
-
-
-// pivot helper 
-// all values less than pivot are to the left
-// all values greater than pivot are to right
-// not create a new array
-// when done return the index of pivot
-
-// let arr = [ 5,2,1,8,4,7,6,3]  // pivot is 5
-// pivot(arr);   returns 4;  the index of where pivot goes
-
-function pivot(arr, start = 0, end = arr.length + 1) {
-    let pivot = arr[start];
-    let swapIdx = start;
-
-    function swap(array, i, j) {
-        const temp = array[i];
-        array[i] = array[j];
-        array[j] = temp;
-    }
-
-    for (let i = start + 1; i < arr.length; i++) {
-        if (pivot > arr[i]) {
-            swapIdx++;
-            swap(arr, swapIdx, i)
+function merge(left, right) {
+    const arr = [];
+    while (left.length && right.length) {
+        if (left[0] < right[0]) {
+            arr.push(left.shift());
+        } else {
+            arr.push(right.shift());
         }
     }
-    swap(arr, start, swapIdx)
-    return swapIdx;
+    return [...arr, ...left, ...right];
 }
-pivot([4, 8, 2, 1, 5, 7, 6, 3])
-// [4, 2, 8, 1, 5, 7, 6, 3]
-// [4, 2, 1, 8, 5, 7, 6, 3]
-// [4, 2, 1, 3, 5, 7, 6, 8]
-// [3, 2, 1, 4, 5, 7, 6, 8]
-// 3 
-// [1,2,3,4,5,6,7,8]
-
+function mergeSort(arr) {
+    const mid = Math.floor(arr.length / 2);
+    const left = arr.slice(0, mid);
+    const right = arr.slice(mid);
+    return merge(mergeSort(left), mergeSort(right));
+}
