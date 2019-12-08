146dfcdd6af907c40f40f4f18a2002a344be87cb
dkang417 <dkang417@gmail.com>
2019-12-08 16:32:55 -0500

    update

--- queuefroms.js
+++ queuefroms.js
@@ -35,6 +35,7 @@ class Queue {
 
 }
 
+
 class Queue {
     constructor() {
         this.first = new Stack();
@@ -85,3 +86,31 @@ class Queue {
 }
 
 module.exports = Queue;
+class Queue {
+    constructor() {
+        this.data = [];
+    }
+    add(record) {
+        this.data.unshift(record);
+    }
+    remove() {
+        return this.data.pop();
+    }
+    peek() {
+        return this.data[this.data.length - 1];
+    }
+}
+class Stack {
+    constructor() {
+        this.data = [];
+    }
+    push(record) {
+        this.data.push(record);
+    }
+    remove() {
+        return this.data.pop();
+    }
+    peek() {
+        return this.data[this.data.length - 1];
+    }
+}
