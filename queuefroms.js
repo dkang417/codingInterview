88d0c3c5e12e02646c3c47ceba68388289aa8929
dkang417 <dkang417@gmail.com>
2019-12-09 21:35:14 -0500

    update

--- queuefroms.js
+++ queuefroms.js
@@ -26,6 +26,25 @@ class Queue {
         while (this.first.peek()) {
             this.second.push(this.first.pop());
         }
+        const result = this.second.pop();
+        while (this.second.peek()) {
+            this.first.push(this.second.pop());
+        }
+        return result;
+    }
+}
+class Queue {
+    constructor() {
+        this.first = new Stack();
+        this.second = new Stack();
+    }
+    add(record) {
+        this.first.push(record);
+    }
+    remove() {
+        while (this.first.peek()) {
+            this.second.push(this.first.pop());
+        }
         const record = this.second.pop();
         while (this.second.peek()) {
             this.first.push(this.second.pop());
@@ -76,7 +95,7 @@ class Queue {
         while (this.first.peek()) {
             this.second.push(this.first.pop());
         }
-        const record = this.second.pop();
+        const record = this.second.peek();
         while (this.second.peek()) {
             this.first.push(this.second.pop());
         }
