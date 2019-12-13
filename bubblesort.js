6876e3a98f3cf3a59d26c248bf2dc98c8deece4a
dkang417 <dkang417@gmail.com>
2019-12-12 20:55:16 -0500

    update

--- bubblesort.js
+++ bubblesort.js
@@ -387,3 +387,27 @@ function selectionSort(arr) {
 
 }
 
+function merge(left, right) {
+    const arr = [];
+    while (left.length && right.length) {
+        if (left[0] < right[0]) {
+            arr.push(left.shift());
+        } else {
+            arr.push(right.shift());
+        }
+    }
+    return [...arr, ...left, ...right];
+}
+
+function mergeSort(arr) {
+
+    if (arr.length === 1) {
+        return arr;
+    }
+    const mid = Math.floor(arr.length / 2);
+    const left = arr.slice(0, mid);
+    const right = arr.slice(mid);
+
+    return merge(merSort(left), mergeSort(right));
+}
+
