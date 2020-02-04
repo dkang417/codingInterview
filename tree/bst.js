3a2cfd29aa10d3cdbdbd61a801e167add0e77ac9
dkang417 <dkang417@gmail.com>
2020-02-04 13:41:41 -0500

    update

--- tree/bst.js
+++ tree/bst.js
@@ -120,21 +120,6 @@ class Node {
 }
 
 
-function validate(node, min = null, max = null) {
-    if (max !== null && node.data > max) {
-        return false;
-    }
-    if (min !== null && node.data < min) {
-        return false;
-    }
-    if (node.left && !validate(node.left, min, node.data)) {
-        return false;
-    }
-    if (node.right && !validate(node.right, node.data, max)) {
-        return false;
-    }
-    return true;
-}
 
 function validate(node, min = null, max = null) {
     if (min !== null && node.data < min) {
@@ -153,15 +138,53 @@ function validate(node, min = null, max = null) {
 }
 
 
+class Node {
+    constructor(data) {
+        this.data = data;
+        this.left = null;
+        this.right = null;
+    }
+    insert(data) {
+        if (data < this.data && this.left) {
+            this.left.insert(data);
+        }
+        else if (data < this.data) {
+            this.left = new Node(data);
+        }
+        else if (data > this.data && this.right) {
+            this.right.insert(data);
+        }
+        else if (data > this.data) {
+            this.right = new Node(data);
+        }
+    }
+    contains(data) {
+        if (data === this.data) {
+            return this;
+        }
+        if (data < this.data && this.left) {
+            return this.left.contains(data);
+        }
+        else if (data > this.data && this.right) {
+            return this.right.contains(data);
+        }
+        return false;
 
-// we need to have min and max values when we recurse through the node- starts with null
-// if there is a min value  and node.data is less than min return false
-// if there is a max value and node.data is greater than max reuturn false
-// recurse through node.left && !validate passing values
-// recurse through node.right && !validate passing values  
-// if it passes through all checks return true
+    }
+}
+function validate(node, min = null, max = null) {
+    if (min !== null && node.data < min) {
+        return false;
+    }
+    if (max !== null && node.data > max) {
+        return false;
+    }
+    if (node.left && !validate(node.left, min, max = node.data)) {
+        return false;
+    }
+    if (node.right && !validate(node.right, node.data, max)) {
+        return false;
+    }
+    return true;
 
-
-//  10
-// 5   12  
-// params = node, min = null, max = null, recurse 
+}
