c1a42fd1a036358b4c8314944f2f9b5f3dedddf8
dkang417 <dkang417@gmail.com>
2020-03-12 19:58:03 -0400

    update

--- tree/bst.js
+++ tree/bst.js
@@ -227,7 +227,7 @@ function validate(node, min = null, max = null) {
     if (max !== null && node.data > max) {
         return false;
     }
-    if (node.left && !validate(node.left, min, max = node.data)) {
+    if (node.left && !validate(node.left, min, node.data)) {
         return false;
     }
     if (node.right && !validate(node.right, node.data, max)) {
