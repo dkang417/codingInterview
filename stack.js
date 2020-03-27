b0a81f9ec56de62740d14cd4b1fcf79d32819e80
dkang417 <dkang417@gmail.com>
2020-03-26 20:22:33 -0400

    update

--- stack.js
+++ stack.js
@@ -79,117 +79,17 @@ class Stack {
         return this.data[this.data.length - 1];
     }
 }
-
-module.exports = Stack;
-
-
-
-// [0:'zero', 1:'two', 2:'nine'];
-// length: 3
-
-//  {0:'one', 2:'two', 3:'three'}
-
-
-
-
-
-
 class Stack {
     constructor() {
-        this.storage = {};
-        this.length = 0;
-    }
-
-    push(value) {
-        this.storage[this.length] = value;
-        this.length++;
-    }
-
-    pop() {
-        if (this.length) {
-            const last = this.storage[this.length - 1];
-            this.storage[this.length - 1] = null;
-            this.length--;
-            return last;
-        }
-
-    }
-    peek() {
-        if (this.length) {
-            const last = this.storage[this.length - 1];
-            return last;
-        }
-    }
-}
-
-const myStack = new Stack();
-myStack.push('zero');
-myStack.push('one');
-myStack.pop();
-myStack.push('twelve')
-console.log(myStack.peek());
-console.log(myStack);
-
-
-//object
-class Stack {
-    constructor() {
-        this.data = {};
-        this.length = 0;
+        this.data = [];
     }
     add(record) {
-        this.data[this.length] = record;
-        this.length++;
+        this.data.push(record);
     }
-    remove() {
-        if (this.length) {
-            const temp = this.data[this.length - 1];
-            this.data[this.length - 1] = null;
-            this.length--;
-            return temp;
-        }
-
+    pop() {
+        return this.data.pop();
     }
     peek() {
-        if (this.length) {
-            const temp = this.data[this.length - 1];
-            return temp;
-        }
-
+        return this.data[this.data.length - 1];
     }
 }
-// object
-class Queue {
-    constructor() {
-        this.storage = {};
-        this.length = 0;
-        this.head = 0;
-    }
-    enqueue(record) {
-        this.storage[this.length + this.head] = record;
-        this.length++;
-
-    }
-    [1, 2, 3, 5]
-    dequeue() {
-        if (this.length) {
-            const remove = this.storage[this.head];
-            this.length--;
-            this.storage[this.head] = null;
-            this.head++;
-            return remove;
-        }
-
-
-    }
-
-}
-
-// [0:'zero', 1:'two', 2:'nine'];
-    // length: 3
-
-  //  {0:'one', 2:'two', 3:'three'}
-
-
-
-
