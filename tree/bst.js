0fff6fd04e1cab555dbfff4da047700ac856ffcd
dkang417 <dkang417@gmail.com>
2020-04-02 21:01:08 -0400

    update

--- tree/bst.js
+++ tree/bst.js
@@ -313,6 +313,19 @@ function validate(node, min = null, max = null) {
         return false;
     }
     return true;
-
-
+}
+function validate(node, min = null, max = null) {
+    if (node.data < min && min !== null) {
+        return false;
+    }
+    if (node.data > max && max !== null) {
+        return false;
+    }
+    if (node.left && !validate(node.left, min, node.data)) {
+        return false;
+    }
+    if (node.right && !validate(node.right, node.data, max)) {
+        return false;
+    }
+    return true;
 }
