d0dac344fcb13b0a2d0d8859c188f041d3d90a3f
dkang417 <dkang417@gmail.com>
2020-04-12 21:01:24 -0400

    update

--- queue.js
+++ queue.js
@@ -95,4 +95,33 @@ class Queue {
     }
 }
 
+// first in first out 
+class Queue {
+    constructor() {
+        this.data = [];
+    }
+    add(record) {
+        this.data.unshift(record);
+    }
+    remove() {
+        return this.data.pop();
+    }
+    peek() {
+        return this.data[this.data.length - 1];
+    }
+}
 
+class Stack {
+    constructor() {
+        this.data = [];
+    }
+    add(record) {
+        this.data.push(record);
+    }
+    remove() {
+        return this.data.pop();
+    }
+    peek() {
+        return this.data[this.data.length - 1];
+    }
+}
