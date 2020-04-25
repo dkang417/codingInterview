b6c3c4223b40c5cac4b1755ca73cb8647a12062e
dkang417 <dkang417@gmail.com>
2020-04-24 21:12:01 -0400

    update

--- sorts.js
+++ sorts.js
@@ -169,7 +169,21 @@ function selectionSort(arr) {
 // works by decomposing arrays into smaller arrays of 0 or 1 then merge them 
 // big o of mergesort-  O(n log n) 
 
+function merge(left, right) {
+    const arr = [];
+    while (left.length && right.length) {
+        if (left[0] < right[0]) {
+            arr.push(left.shift());
+        } else {
+            arr.push(right.shift());
+        }
+    }
+    return [...arr, ...left, ...right];
+}
 
+function mergeSort(arr) {
+
+}
 function merge(left, right) {
     const arr = [];
     while (left.length && right.length) {
@@ -236,6 +250,27 @@ function mergeSort(arr) {
     }
     return merge(mergeSort(left), mergeSort(right));
 }
+function merge(left, right) {
+    const arr = [];
+    while (left.length && right.length) {
+        if (left[0] < right[0]) {
+            arr.push(left.shift());
+        } else {
+            arr.push(right.shift());
+        }
+    }
+    return [...arr, ...left, ...right];
+}
+function mergeSort(arr) {
+    if (arr.length === 1) {
+        return arr;
+    }
+    const mid = Math.floor(arr.length / 2);
+    const left = arr.slice(0, mid);
+    const right = arr.slice(mid);
+    return merge(mergeSort(left), mergeSort(right));
+}
+
 function merge(left, right) {
     const arr = [];
     while (left.length && right.length) {
