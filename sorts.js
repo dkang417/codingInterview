c5584f49b664fded3ce6788ca7d46460dbf3e3d1
dkang417 <dkang417@gmail.com>
2020-04-25 20:33:15 -0400

    update

--- sorts.js
+++ sorts.js
@@ -291,3 +291,24 @@ function mergeSort(arr) {
     const right = arr.slice(mid);
     return merge(mergeSort(left), mergeSort(right));
 }
+function merge(left, right) {
+    const arr = [];
+    while (left.length && right.length) {
+        if (left[0] < right[0]) {
+            arr.push(left.shift());
+        } else {
+            arr.push(right.shift());
+        }
+    }
+    return [...arr, ...left, ...right];
+}
+function mergeSort(arr) {
+    if (arr.length === 1) {
+        return arr;
+    }
+    const mid = Math.floor(arr.length / 2);
+    const left = arr.slice(0, mid);
+    const right = arr.slice(mid);
+
+    return merge(mergeSort(left), mergeSort(right));
+}
