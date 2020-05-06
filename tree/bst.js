7821bb452c5b53012c7429cad1d60fc0d1f110ba
dkang417 <dkang417@gmail.com>
2020-05-05 20:48:04 -0400

    update

--- tree/bst.js
+++ tree/bst.js
@@ -16,11 +16,72 @@ class Node {
         this.left = null;
         this.right = null;
     }
+    insert(data) {
+        if (data > this.data && this.right) {
+            this.right.insert(data);
+        }
+        else if (data > this.data) {
+            this.right = new Node(data);
+        }
+        else if (data < this.data && this.left) {
+            this.left.insert(data);
+        }
+        else if (data < this.data) {
+            this.left = new Node(data);
+        }
+    }
+    contains(data) {
+        if (data === this.data) {
+            return true;
+        }
+        if (data > this.data && this.right) {
+            return this.right.contains(data);
+        }
+        else if (data < this.data && this.left) {
+            return this.left.contains(data);
+        }
+        return null;
+    }
+}
+function validate(node, min = null, max = null) {
+    if (min !== null && node.data < min) {
+        return false;
+    }
+    if (max !== null && node.data > max) {
+        return false;
+    }
+    if (node.left && !validate(node.left, min, node.data)) {
+        return false;
+    }
+    if (node.right && !validate(node.right, node.data, max)) {
+        return false;
+    }
+    return true;
+}
+
+
+function validate(node, min = null, max = null) {
+    if (min !== null && node.data < min) {
+        return false;
+    }
+    if (max !== null && node.data > max) {
+        return false;
+    }
+    if (node.left && !validate(node.left, min, node.data)) {
+        return false;
+    }
+    if (node.right && !validate(node.right, node.data, max)) {
+        return false;
+    }
+    return true;
+}
+class Node {
     constructor(data) {
         this.data = data;
         this.left = null;
         this.right = null;
     }
+
     insert(data) {
         if (data > this.data && this.right) {
             this.right.insert(data);
