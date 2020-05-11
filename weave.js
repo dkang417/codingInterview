8a146f9d7a9cde3e73c3fe801a5ec2f966ab4f1b
dkang417 <dkang417@gmail.com>
2020-05-11 19:04:38 -0400

    update

--- weave.js
+++ weave.js
@@ -28,6 +28,19 @@ const Queue = require('./queue');
 
 function weave(sourceOne, sourceTwo) {
     const q = new Queue();
+    while (sourceOne.peek() || sourceTwo.peek()) {
+        if (sourceOne.peek()) {
+            q.add(sourceOne.remove());
+        }
+        if (sourceTwo.peek()) {
+            q.add(sourceTwo.remove());
+        }
+    }
+    return q;
+}
+
+function weave(sourceOne, sourceTwo) {
+    const q = new Queue();
     while (sourceOne.peek() || sournceTwo.peek()) {
         if (sournceOne.peek()) {
             q.add(sourceOne.remove());
