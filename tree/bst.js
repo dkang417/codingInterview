f44f72a7cfb4c10595c0883abaab36fbb2146eeb
dkang417 <dkang417@gmail.com>
2020-05-17 20:28:26 -0400

    update

--- tree/bst.js
+++ tree/bst.js
@@ -10,6 +10,56 @@
 // 3) Implement the 'contains' method for the Node
 // class.  Contains should accept a 'data' argument
 // and return the Node in the tree with the same value.
+
+class Node {
+    constructor(data) {
+        this.data = data;
+        this.left = null;
+        this.right = null;
+    }
+    insert(data) {
+        if (data > this.data && this.right) {
+            this.right.insert(data);
+        }
+        else if (data > this.data) {
+            this.right = new Node(data);
+        }
+        else if (data < this.data && this.left) {
+            this.left.insert(data);
+        }
+        else if (data < this.data) {
+            this.left = new Node(data);
+        }
+    }
+    contains(data) {
+        if (data === this.data) {
+            return true;
+        }
+        if (data > this.data && this.right) {
+            return this.right.contains(data);
+        }
+        else if (data < this.data && this.left) {
+            return this.left.contains(data);
+        }
+        return null;
+    }
+}
+function validate(node, min = null, max = null) {
+    if (node.data > max && max !== null) {
+        return false;
+    }
+    if (node.data < min && min !== null) {
+        return false;
+    }
+    if (node.left && !validate(node.left, min, node.data)) {
+        return false;
+    }
+    if (node.right && !validate(node.right, node.data, max)) {
+        return false;
+    }
+    return true;
+}
+
 class Node {
     constructor(data) {
         this.data = data;
