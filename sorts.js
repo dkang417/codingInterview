6197b4933f5fa9e5c7c0b19e79f6773d52c92839
dkang417 <dkang417@gmail.com>
2020-06-09 19:27:31 -0400

    update

--- sorts.js
+++ sorts.js
@@ -21,6 +21,19 @@ function bubbleSort(arr) {
         }
     }
     return arr;
+
+}
+function bubbleSort(arr) {
+    for (let i = 0; i < arr.length; i++) {
+        for (let j = 0; j < arr.length - i - 1; j++) {
+            if (arr[j + 1] < arr[j]) {
+                const temp = arr[j];
+                arr[j] = arr[j + 1];
+                arr[j + 1] = temp;
+            }
+        }
+    }
+    return arr;
 }
 function bubbleSort(arr) {
     for (let i = 0; i < arr.length; i++) {
@@ -79,7 +92,22 @@ function bubbleSort(arr) {
     }
     return arr;
 }
-
+function selectionSort(arr) {
+    for (let i = 0; i < arr.length; i++) {
+        let lesser = i;
+        for (let j = i + 1; j < arr.length; j++) {
+            if (arr[j] < arr[lesser]) {
+                lesser = j;
+            }
+        }
+        if (lesser !== i) {
+            let temp = arr[i];
+            arr[i] = arr[lesser];
+            arr[lesser] = temp;
+        }
+    }
+    return arr;
+}
 function selectionSort(arr) {
     for (let i = 0; i < arr.length; i++) {
         let least = i;
@@ -334,4 +362,26 @@ function mergeSort(arr) {
     const right = arr.slice(mid);
     return merge(mergeSort(left), mergeSort(right));
 
+}
+
+function merge(left, right) {
+    const arr = [];
+    while (left.length && right.length) {
+        if (left[0] < right[0]) {
+            arr.push(left.shift());
+        } else {
+            arr.push(right.shift());
+        }
+    }
+    return [...arr, ...left, ...right];
+}
+
+function mergeSort(arr) {
+    if (arr.length === 1) {
+        return arr;
+    }
+    const mid = Math.floor(arr.length / 2);
+    const left = arr.slice(0, mid);
+    const right = arr.slice(mid);
+    return merge(mergeSort(left), mergeSort(right));
 }
