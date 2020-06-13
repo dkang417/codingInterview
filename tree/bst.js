9908941237444c370ead89e7d773de5a1db89960
dkang417 <dkang417@gmail.com>
2020-06-13 19:43:12 -0400

    update

--- tree/bst.js
+++ tree/bst.js
@@ -17,7 +17,18 @@ class Node {
         this.right = null;
     }
     insert(data) {
-
+        if (data > this.data && this.right) {
+            this.right.insert(data);
+        }
+        else if (data > this.data) {
+            this.right = new Node(data);
+        }
+        else if (data < this.data && this.left) {
+            this.left.insert(data);
+        }
+        else if (data < this.data) {
+            this.left = new Node(data);
+        }
     }
     contains(data) {
 
@@ -153,6 +164,21 @@ function validate(node, min = null, max = null) {
     }
     if (max !== null && node.data > max) {
         return false;
+    }
+    if (node.left && !validate(node.left, min, node.data)) {
+        return false;
+    }
+    if (node.right && !validate(node.right, node.data, max)) {
+        return false;
+    }
+    return true;
+}
+function validate(node, min = null, max = null) {
+    if (min !== null && node.data < min) {
+        return false;
+    }
+    if (max !== null && node.data > max) {
+        return fasle;
     }
     if (node.left && !validate(node.left, min, node.data)) {
         return false;
