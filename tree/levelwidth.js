3507feee14cf221824a18815aeb61d917f9a7cc3
dkang417 <dkang417@gmail.com>
2020-06-15 19:08:36 -0400

    update

--- tree/levelwidth.js
+++ tree/levelwidth.js
@@ -10,7 +10,21 @@
 // |       |
 // 4       5
 // Answer: [1, 3, 2]
-
+function levelWidth(root) {
+    const arr = [this.root, 's'];
+    const count = [0];
+    while (arr.length > 1) {
+        const node = arr.shift();
+        if (node === 's') {
+            arr.push('s');
+            count.push(0);
+        } else {
+            count[count.length - 1]++;
+            arr.push(...node.children);
+        }
+    }
+    return count;
+}
 function levelWidth(root) {
     const arr = [this.root, 's'];
     const count = [0];
