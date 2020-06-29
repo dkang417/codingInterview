145eefd13b094fcc849b127c1cb82354548a4be2
dkang417 <dkang417@gmail.com>
2020-06-28 20:56:40 -0400

    update

--- linkedList.js
+++ linkedList.js
@@ -106,6 +106,23 @@ class LinkedList {
         const node = new Node(data, this.prev.next);
         prev.next = node;
     }
+    insertAt(data, index) {
+        if (index === 0) {
+            this.head = new Node(data, this.head);
+            return;
+        }
+        if (!this.head) {
+            this.head = new Node(data);
+            return;
+        }
+        const prev = this.getAt(index - 1) || this.getLast();
+        const node = new node(data, this.prev.next);
+        prev.next = node;
+
+    }
+    removeAt(index) {
+
+    }
 }
 class Node {
     constructor(data, next = null) {
